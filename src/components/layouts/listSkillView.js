7ea5a35f413577810f6feddde2d1b0d393bb2a7f
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2019-07-27 23:48:06 +0700

    Update listSkillView.js

--- src/components/layouts/listSkillView.js
+++ src/components/layouts/listSkillView.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import { Media } from 'reactstrap'
+//import { Link } from 'react-router-dom'
+import { Card, CardTitle, CardText } from 'reactstrap'
 
 const ListSkillView = ({skill}) => {
+    const MAX_LENGTH = 150;
     return(
-        <Media style={{marginBottom:20}}>
+        <Card sm="3">
+            <CardBody>
+                <CardTitle>{skill.titlevideo}</CardTitle>
+            </CardBody>
+            <img width="100%" src={skill.photo} alt={skill.titlevideo} />
+            <CardBody>
+                <CardText>
+                {skill.pagedesc.length > MAX_LENGTH ?
+                    (
+                        <div>
+                        {`${skill.pagedesc.substring(0, MAX_LENGTH)}...`}
+                        </div>
+                    ) :
+                    <p>{skill.pagedesc}</p>
+                }
+                </CardText>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default ListSkillView
+/*
+<Media style={{marginBottom:20}}>
             <Media left >                
                 <Media object src={skill.photo} alt={skill.titlevideo} className="img-thumbnail-rs " />
             </Media>
@@ -11,7 +36,6 @@ const ListSkillView = ({skill}) => {
                 {skill.pagedesc} 
             </Media>
         </Media>
-    )
-}
 
-export default ListSkillView
+
+*/
