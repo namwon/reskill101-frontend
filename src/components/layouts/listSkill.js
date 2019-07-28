198b73a47957b82dfc6c02c40113c693588993d0
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2019-07-28 15:35:28 +0700

    Update listSkill.js

--- src/components/layouts/listSkill.js
+++ src/components/layouts/listSkill.js
@@ -5,24 +5,21 @@ import { Link } from 'react-router-dom'
 
 const ListSkill = ({skill}) => {
     return(
-     
-          <div>
-
-<div className="d-flex" style={{marginBottom:20}}>
-            <Media  left to={'/skill/' + skill.item_id} key={skill.item_id}>
-                <Media object src={skill.photos} alt={skill.title} className="img-thumbnail-rs " />
-            </Media>
-            <Media body>
-                <Link to={'/skill/' + skill.item_id} key={skill.item_id} >
-                    <Media heading className="h5">
-                        {skill.title} 
-                    </Media>
-                    <p>{skill.desc}</p>
-                </Link>
-                
-            </Media>
-        </div>
+        <div>
+            <div className="d-flex" style={{marginBottom:20}}>
+                <Media  left to={'/skill/' + skill.item_id} key={skill.item_id}>
+                    <Media object src={skill.photos} alt={skill.title} className="img-thumbnail-rs " />
+                </Media>
+                <Media body>
+                    <Link to={'/skill/' + skill.item_id} key={skill.item_id} >
+                        <Media heading className="h5">
+                            {skill.title} 
+                        </Media>
+                        <p>{skill.desc}</p>
+                    </Link>
+                </Media>
             </div>
+        </div>
     )
 }
 
