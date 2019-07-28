a2cfae10ce6acd66c9cbbb3bacd7357e5e728d80
Sutthanit Chaikhunpol <namwon74@hotmail.com>
2019-07-28 16:38:59 +0700

    Deploy

---
+++ dist/precache-manifest.0e3af5ba656ab19c78c61a830f649cf9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc15066405a6184925f434471fe7816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d653ac2601f3ac40db38",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d6eacd30f457542cfdca",
+    "url": "/static/css/main.9d525c3c.chunk.css"
+  },
+  {
+    "revision": "d653ac2601f3ac40db38",
+    "url": "/static/js/2.06d713e6.chunk.js"
+  },
+  {
+    "revision": "d6eacd30f457542cfdca",
+    "url": "/static/js/main.ccd68eb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29e29d3cee104b818625ff7eba6e3c4c",
+    "url": "/static/media/DBHelvethaicaX-55Regular.29e29d3c.eot"
+  },
+  {
+    "revision": "3f304e6321afa961fee73f6bbe9c442b",
+    "url": "/static/media/DBHelvethaicaX-55Regular.3f304e63.woff"
+  },
+  {
+    "revision": "940f480ee74cf7ba540b68ec65add29c",
+    "url": "/static/media/DBHelvethaicaX-55Regular.940f480e.woff2"
+  },
+  {
+    "revision": "f71539381e48974ee6f9c52bfcee8a56",
+    "url": "/static/media/DBHelvethaicaX-55Regular.f7153938.ttf"
+  },
+  {
+    "revision": "159ae9eac1c1515fc755c804936caa17",
+    "url": "/static/media/banner01@2x.159ae9ea.png"
+  },
+  {
+    "revision": "7c6952647161c2049e534a354b40bdec",
+    "url": "/static/media/banner@2x.7c695264.png"
+  },
+  {
+    "revision": "ff1cc3ccb99c33d74f4fe941c963a51e",
+    "url": "/static/media/bn-colour-dark@2x.ff1cc3cc.png"
+  },
+  {
+    "revision": "8413554a9ed98ca5d78e3801dce6fd5c",
+    "url": "/static/media/image6@2x.8413554a.png"
+  }
+]);
